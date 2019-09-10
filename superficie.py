d3ae97188a4144a5528fd293502fd59379ead339
yugovtr <vtrhg69@hotmail.com>
2019-09-10 12:29:54 -0300

    superficie

--- superficie.py
+++ superficie.py
@@ -1,104 +1,146 @@
 from OpenGL.GL import *
-from OpenGL.GLUT import *
 from OpenGL.GLU import *
-import numpy as np 
+from OpenGL.GLUT import *
 
-ctlpoints = np.empty((4,4,3))
-showPoints = 0
-theNurb = gluNewNurbsRenderer()
+from numpy import array 
+import sys, random
 
-def init_surface():
-    for u in range(4):
-        for v in range(4):
-            ctlpoints[u][v][0] = 2.0*(u - 1.5)
-            ctlpoints[u][v][1] = 2.0*(v - 1.5)
-
-            if ( (u == 1 or u == 2) and (v == 1 or v == 2)):
-                ctlpoints[u][v][2] = 3.0
-            else:
-                ctlpoints[u][v][2] = -3.0
-
-def nurbsError(errorCode):
-   estring = gluErrorString(errorCode)
-   print("Nurbs Error: {0}\n".format(estring)) 
-
-def init():
-    mat_diffuse = [ 0.7, 0.7, 0.7, 1.0 ]
-    mat_specular = [ 1.0, 1.0, 1.0, 1.0 ]
-    mat_shininess = [ 100.0 ]
-
-    glClearColor (0.0, 0.0, 0.0, 0.0)
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse)
-    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular)
-    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess)
-
-    glEnable(GL_LIGHTING)
-    glEnable(GL_LIGHT0)
-    glEnable(GL_DEPTH_TEST)
-    glEnable(GL_AUTO_NORMAL)
-    glEnable(GL_NORMALIZE)
-
-    init_surface()
-
-    theNurb = gluNewNurbsRenderer()
-    gluNurbsProperty(theNurb, GLU_SAMPLING_TOLERANCE, 25.0)
-    gluNurbsProperty(theNurb, GLU_DISPLAY_MODE, GLU_FILL)
-    gluNurbsCallback(theNurb, GLU_ERROR, nurbsError)
-
+THE_LIST = None
 
 def display():
-    knots = [0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0]
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+	glutSetWindow( context )
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+	glEnable(GL_LIGHT0)
+	glEnable(GL_LIGHTING)
+	glDisable( GL_CULL_FACE )
+	glEnable(GL_DEPTH_TEST)
+	glCallList(THE_LIST)
+	glutSwapBuffers()
 
-    glPushMatrix()
-    glRotatef(330.0, 1.,0.,0.)
-    glScalef (0.5, 0.5, 0.5)
+def main():
+	mat_red_diffuse = array( ( 0.7, 0.0, 0.1, 1.0 ), 'f' )
+	mat_green_diffuse = array(( 0.0, 0.7, 0.1, 1.0 ),'f')
+	mat_blue_diffuse = array(( 0.0, 0.1, 0.7, 1.0 ),'f')
+	mat_yellow_diffuse = array(( 0.7, 0.8, 0.1, 1.0 ),'f')
+	mat_specular = array(( 1.0, 1.0, 1.0, 1.0 ),'f')
+	mat_shininess = GLfloat( 100.0 )
+	knots = array( (0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0),'f' )
 
-    gluBeginSurface(theNurb)
-    gluNurbsSurface(theNurb, 8, knots, 8, knots, 4 * 3, 3, ctlpoints[0][0][0], 4, 4, GL_MAP2_VERTEX_3)
-    gluEndSurface(theNurb)
+	glutInit(sys.argv)
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
+	global context
+	context = glutCreateWindow('Superficie')
+	glutSetWindow( context )
 
-    if showPoints:
-        glPointSize(5.0)
-        glDisable(GL_LIGHTING)
-        glColor3f(1.0, 1.0, 0.0)
-        glBegin(GL_POINTS)
-    
-        for i in range(4):
-            for j in range(4):
-                glVertex3f(ctlpoints[i][j][0], ctlpoints[i][j][1], ctlpoints[i][j][2])
+	nurb = gluNewNurbsRenderer()
+	# get a really good sampling
+	gluNurbsProperty(nurb, GLU_SAMPLING_TOLERANCE, 5.0)
+	gluNurbsProperty(nurb, GLU_DISPLAY_MODE, GLU_FILL) # GLU_OUTLINE_POLYGON
 
-        glEnd()
-        glEnable(GL_LIGHTING)
-   
-    glPopMatrix()
-    glFlush()
+	# Build control points for NURBS mole hills.
+	pts1 = []
+	pts2 = []
+	pts3 = []
+	pts4 = []
+
+	for u in range(4):
+		pts1.append([])
+		pts2.append([])
+		pts3.append([])
+		pts4.append([])
+		for v in range(4):
+			# Red.
+			pts1[u].append([2.0*u, 2.0*v, 0.0])
+			if (u == 1 or u == 2) and (v == 1 or v == 2):
+				pts1[u][v][2] = 3.0
+
+			# Green.
+			pts2[u].append([2.0*u - 6.0, 2.0*v - 6.0, 0.0])
+			if (u == 1 or u == 2) and (v == 1 or v == 2):
+				if u == 1 and v == 1:
+					# Pull hard on single middle square.
+					pts2[u][v][2] = 15.0
+				else:
+					# Push down on other middle squares.
+					pts2[u][v][2] = -2.0
+
+			# Blue.
+			pts3[u].append([2.0*u - 6.0, 2.0*v, 0.0])
+			if (u == 1 or u == 2) and (v == 1 or v == 2):
+				if u == 1 and v == 2:
+					# Pull up on single middple square.
+					pts3[u][v][2] = 11.0
+				else:
+					# Pull up slightly on other middle squares.
+					pts3[u][v][2] = 2.0
+
+			# Yellow.
+			pts4[u].append([2.0*u, 2.0*v - 6.0, 0.0])
+			if u != 0 and (v == 1 or v == 2):
+				if v == 1:
+					# Push down front middle and right squares.
+					pts4[u][v][2] = -2.0
+				else:
+					# Pull up back middle and right squares.
+					pts4[u][v][2] = 5.0
 
 
-def reshape(w, h):
-    glViewport(0, 0, w, h)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective (45.0, w/h, 3.0, 8.0)
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
-    glTranslatef (0.0, 0.0, -5.0)
+	# Stretch up red's far right corner.
+	pts1[3][3][2] = 6.0
+	# Pull down green's near left corner a little.
+	pts2[0][0][2] = -2.0
+	# Turn up meeting of four corners.
+	pts1[0][0][2] = 1.0
+	pts2[3][3][2] = 1.0
+	pts3[3][0][2] = 1.0
+	pts4[0][3][2] = 1.0
 
-def keyboard(key, x, y):
-    if key == 'c':
-        showPoints = not showPoints
-        glutPostRedisplay()
-    elif ord(key) == 27:
-        sys.exit(0) 
+	pts1,pts2,pts3,pts4 = array(pts1,'f'),array(pts2,'f'),array(pts3,'f'),array(pts4,'f')
+	glMatrixMode(GL_PROJECTION)
+	gluPerspective(55.0, 1.0, 2.0, 24.0)
+	glMatrixMode(GL_MODELVIEW)
+	glTranslatef(0.0, 0.0, -15.0)
+	glRotatef(330.0, 1.0, 0.0, 0.0)
 
-if __name__ == "__main__":
-    glutInit()
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH)
-    glutInitWindowSize (500, 500)
-    glutInitWindowPosition (100, 100)
-    glutCreateWindow("Superficie")
-    init()
-    glutReshapeFunc(reshape)
-    glutDisplayFunc(display)
-    glutKeyboardFunc (keyboard)
-    glutMainLoop()
+	global THE_LIST
+	THE_LIST = glGenLists( 1 )
+	glNewList(THE_LIST, GL_COMPILE)
+
+	glEnable(GL_NORMALIZE)
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular)
+	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess)
+
+	# Render red hill.
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_red_diffuse)
+	gluBeginSurface(nurb)
+	gluNurbsSurface(nurb, knots, knots, pts1, GL_MAP2_NORMAL)
+	gluNurbsSurface(nurb, knots, knots, pts1, GL_MAP2_VERTEX_3)
+	gluEndSurface(nurb)
+
+	# Render green hill.
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_green_diffuse)
+	gluBeginSurface(nurb)
+	gluNurbsSurface(nurb, knots, knots, pts2, GL_MAP2_NORMAL)
+	gluNurbsSurface(nurb, knots, knots, pts2, GL_MAP2_VERTEX_3)
+	gluEndSurface(nurb)
+
+	# Render blue hill.
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_blue_diffuse)
+	gluBeginSurface(nurb)
+	gluNurbsSurface(nurb, knots, knots, pts3, GL_MAP2_NORMAL)
+	gluNurbsSurface(nurb, knots, knots, pts3, GL_MAP2_VERTEX_3)
+	gluEndSurface(nurb)
+
+	# Render yellow hill.
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_yellow_diffuse)
+	gluBeginSurface(nurb)
+	gluNurbsSurface(nurb, knots, knots, pts4, GL_MAP2_NORMAL)
+	gluNurbsSurface(nurb, knots, knots, pts4, GL_MAP2_VERTEX_3)
+	gluEndSurface(nurb)
+	glEndList()
+
+	glutDisplayFunc(display)
+
+if __name__ == '__main__':
+	main()
+	glutMainLoop()
