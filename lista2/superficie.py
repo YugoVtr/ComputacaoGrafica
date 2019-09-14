6d3e8aeeadd9e30b49ce75d965f17af0b61afd39
yugovtr <vtrhg69@hotmail.com>
2019-09-13 21:15:51 -0300

    Correcoes

--- lista2/superficie.py
+++ lista2/superficie.py
@@ -7,19 +7,17 @@ from numpy import array, zeros
 import sys, os, random 
 
 points = zeros((4,4,3))
-show = 0
 nurb = None
 
-def init_surface():
-    global points, nurb, show
+def surface():
+    global points, nurb
     for u in range(4):
+        points[0][u][2] = -2.0  
         for v in range(4):
             points[u][v][0] = 2.0 * (u - 1.5)
             points[u][v][1] = 2.0 * (v - 1.5)  
             if (u == 1 or u == 2) and (v == 1 or v == 2):
-			    points[u][v][2] = 4.0       
-    for u in range(4): 
-        points[0][u][2] = -2.0   
+			    points[u][v][2] = 4.0               
 
 def errors(errorCode):
     estring = gluErrorString(errorCode)
@@ -27,7 +25,7 @@ def errors(errorCode):
     sys.exit(0)
 
 def init():
-    global points, nurb, show
+    global points, nurb
     mat_diffuse = array((0.7, 0.7, 0.7, 1.0), 'f')
     mat_specular = array((1.0, 1.0, 1.0, 1.0), 'f')
     mat_shininess = array((100.0), 'f')
@@ -43,7 +41,7 @@ def init():
     glEnable(GL_AUTO_NORMAL)
     glEnable(GL_NORMALIZE)
 
-    init_surface()
+    surface()
 
     nurb = gluNewNurbsRenderer()
     gluNurbsProperty(nurb, GLU_SAMPLING_TOLERANCE, 1.0)
@@ -51,7 +49,7 @@ def init():
     gluNurbsCallback(nurb, GLU_ERROR, errors)
 
 def display(): 
-    global points, nurb, show
+    global points, nurb
     knots = array((0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0), 'f')
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
@@ -62,16 +60,6 @@ def display():
     gluNurbsSurface(nurb, knots, knots, points, GL_MAP2_VERTEX_3)
     gluEndSurface(nurb)
 
-    if show:
-        glPointSize(5.0)
-        glDisable(GL_LIGHTING)
-        glColor3f(1.0, 1.0, 0.0)
-        glBegin(GL_POINTS)
-        for i in range(4):
-            for j in range(4):
-                glVertex3f(points[i][j][0], points[i][j][1], points[i][j][2])
-        glEnd()
-        glEnable(GL_LIGHTING)
     glPopMatrix()
     glFlush()
 
@@ -85,17 +73,13 @@ def reshape(w, h):
     glTranslatef (0.0, 0.0, -5.0)
 
 def keyboard(key, x, y):
-    global show
-    if key == 'c': 
-        show = not show
-        glutPostRedisplay()
-    elif ord(key) == 27:
+    if ord(key) == 27:
         sys.exit(0)
 
 def main(): 
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH)
-    glutInitWindowSize (500, 500)
+    glutInitWindowSize (512, 512)
     glutInitWindowPosition (100, 100)
     glutCreateWindow(sys.argv[0])
     init()
